a8650ff958c92abe1b5369b1c0a7307f88d85532
ldrk11 <185111740+ldrk11@users.noreply.github.com>
2024-10-26 22:17:54 -0400

    "start" command now creates a file with island name

--- commands/unorganized/start.js
+++ commands/unorganized/start.js
@@ -3,9 +3,17 @@ const {SlashCommandBuilder} = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('start')
-		.setDescription('Start your island'),
+		.setDescription('Start your island')
+        .addStringOption(option => option.setName("name").setDescription("Enter the name of your new island!").setRequired(true)),
 	async execute(interaction) {
-        interaction.client.write_f("./data/users/test.txt", "hello", false)
-		await interaction.reply(interaction.client.read_f("./data/users/test.txt", false));
+        var island_name = interaction.options.getString("name")
+        if (island_name.length > 20){
+		    await interaction.reply("Island name can't be more than 20 characters");
+        }
+        if (island_name.includes("/") || island_name.includes("\\") || island_name.includes(".")){
+            await interaction.reply("Island name contain and of the following characters: / \\ .");
+        }
+        interaction.client.write_f(`./data/users/${interaction.user.id}/${island_name}.json`, {}, true);
+		await interaction.reply(`${island_name} was created!`);
 	},
 };
